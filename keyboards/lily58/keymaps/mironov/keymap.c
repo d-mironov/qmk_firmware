dd54779c2e3d916b04fd3aca958b02f34a171f28
Daniel Mironov <git@danielmironov.dev>
2025-05-21 00:55:56 +0200

    fixes german letters being independent from OS

--- keyboards/lily58/keymaps/mironov/keymap.c
+++ keyboards/lily58/keymaps/mironov/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, MC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_ESC,   KC_RBRC,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-                               KC_LALT, KC_LGUI, MO(LINUX_SYMBOL_NUMBER), KC_SPC,    KC_ENT,  MO(INTERNATIONAL), KC_BSPC, KC_DEL
+                               KC_LALT, KC_LGUI, MO(WINDOWS_SYMBOL_NUMBER), KC_SPC,    KC_ENT,  MO(INTERNATIONAL), KC_BSPC, KC_DEL
     ),
     /* SYMBOLS & NUMBERS for LINUX
      * ,-----------------------------------------.                    ,-----------------------------------------.
